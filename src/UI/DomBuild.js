2b43357b84c57a9d4d1ed7efe5ae6b865324cac7
Mtende kuyokwa <mtendekuyokwa19@gmail.com>
2024-06-02 21:51:43 +0200

    update: building of the game boards

--- src/UI/DomBuild.js
+++ src/UI/DomBuild.js
@@ -1,3 +1,5 @@
+ import './style.css';
+
 class createElementtoDom{
 
     static domElementCreator(type,newId,parentBox,Words="",placeholderWords=""){
@@ -26,10 +28,37 @@ class createElementtoDom{
   }
   
   export let Gameboards=(()=>{
+let body=document.querySelector('body');
 
-    let userGameBoard=createElementtoDom.domElementCreator('div',"userGameBoard","body");
+  let GameBoardsHolder=createElementtoDom.domElementCreator('div',"gameBoardHolder",body)
+    let userGameBoard=createElementtoDom.domElementCreator('div',"userGameBoard",GameBoardsHolder);
+    let computerGameBoard=createElementtoDom.domElementCreator('div',"computerGameBoard",GameBoardsHolder)
+  let shipBoard=createElementtoDom.domElementCreator('div',"shipBoard",body)  
+
+    return {userGameBoard,computerGameBoard}
+  })()
 
 
-    return {userGameBoard}
+  let userGameBoardManager=(()=>{
+   
+    function shipButtons(board) {
+
+      for (let i = 0; i < 10; i++) {
+        let lineSetGrid=createElementtoDom.domElementCreator("div","lineGrid"+i,board)
+
+        for (let x = 0; x <10; x++) {
+          let button=createElementtoDom.domElementCreator("button","button"+i,lineSetGrid)
+          
+          
+        }
+        
+      }
+      
+
+    }
+
+    shipButtons(Gameboards.userGameBoard);
+    shipButtons(Gameboards.computerGameBoard)
+
   })()
   
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import shipMovement from "./shipMovement";
 import shipClasses from "./ship";
-import Gameboard from './UI/DomBuild.js';
+import  { Gameboards } from './UI/DomBuild.js';
+
 
 // shipMovement.play();
 
